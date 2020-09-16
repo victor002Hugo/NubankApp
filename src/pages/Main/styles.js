0cdeca21b84bc35e5af6013bbd370d13c249aa07
Victor Hugo <victorsouvilela@hotmail.com>
2020-09-16 10:29:38 -0300

    Finalizando o projeto

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Animated } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
@@ -14,12 +15,16 @@ max-height:400px;
 z-index:5;
 `;
 
-export const Card = styled.View`
+export const Card = styled(Animated.View)`
 flex:1;
 background:#FFF;
 border-radius:4px;
 margin:0 20px;
 height:100%;
+position:absolute;
+left:0;
+right:0;
+top:0;
 `;
 
 export const CardHeader = styled.View`
